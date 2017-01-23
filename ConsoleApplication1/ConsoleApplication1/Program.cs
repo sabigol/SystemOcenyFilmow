056b6a51b7989207e043153600dcb992b2d841ab
sabigol <golonka.sabina92@gmail.com>
2017-01-23 23:10:54 +0100

    JSON

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApplication12
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var json = new WebClient().DownloadString("http://www.omdbapi.com/?s=name");
+
+            dynamic stuff = JsonConvert.DeserializeObject(json);
+
+            foreach (var film in stuff.Search)
+            {
+                Console.WriteLine("tytuł filmu: " + film.Title + " Rok powstania: " + film.Year);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
